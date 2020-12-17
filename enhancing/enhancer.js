3d112b8b90f57bbc05090f4b0b07f70d20762ae2
kristapants <peachykeen@gmail.com>
2020-12-16 21:12:36 -0600

    enhancements complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,31 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.enhancement == 20) {
+    return { ...item,};
+  } 
+  return {...item, enhancement: item.enhancement == 20 ? item.enhancement += 1 : 20}
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return {...item,
+      durability: ((item.durability - 5 > 0) ? item.durability -= 5 : 0)
+    }
+  } else if (14 < item.enhancement < 17) {
+    return {...item,
+      durability: ((item.durability - 10 > 0) ? item.durability -= 10 : 0)
+    }
+  } else if (item.enhancement > 16) {
+    return { ...item, 
+      durability: ((item.durability - 10 > 0) ? item.durability -= 10 : 0),
+      enhancement: ((item.enhancement - 1 > 0) ? item.enhancement -= 1 : 0)
+    };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
